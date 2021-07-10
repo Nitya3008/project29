7c00d4bc7062c0d0d6e14580e258b1e897281e36
Nitya3008 <84838358+Nitya3008@users.noreply.github.com>
2021-07-10 12:05:11 +0530

    Add files via upload

---
+++ js/Base.js
@@ -0,0 +1,21 @@
+class Base{
+    constructor(x,y,w,h,color,isStatic){
+        let options={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,w,h,options);
+        this.w=w;
+        this.h=h;
+        this.color=color;
+        World.add(world,this.body);
+    }
+
+    show(){
+        let pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rectMode(CENTER);
+    rect(pos.x,pos.y, this.w, this.h);
+    pop();
+    }
+}
